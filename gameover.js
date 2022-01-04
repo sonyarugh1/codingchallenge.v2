a73ae2a4fa67f21c37e9b3c0968710d9719e0bfd
sonyarugh1 <91481258+sonyarugh1@users.noreply.github.com>
2022-01-03 23:28:40 -0500

    Create gameover.js

---
+++ gameover.js
@@ -0,0 +1,11 @@
+var gameover=function(){
+background(255,255,255);
+document.getElementById("mydiv2").style.visibility=
+  "visible";
+  document.getElementById("mydiv").style.visibility=
+  "hidden";
+  fill(0,0,0);
+  textSize(20);
+  text('Click anywhere to continue',140,450);
+  
+};
