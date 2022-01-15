b1311911d124285173693fe20eb672aa21916822
sonyarugh1 <91481258+sonyarugh1@users.noreply.github.com>
2022-01-14 22:14:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var s;
 var scl = 20;
 //Food variable
 var food;
+var r;
+var g;
+var b;
 //Function to set up canvas and snake
 function setup() {
   createCanvas(520, 520);
@@ -22,52 +25,6 @@ function pickLocation() {
   food.mult(scl);
 
 }
-function foodcolor(){
-  if (scene === 2 &&     
-    mouseX < 95 &&
-    mouseX > 75 &&
-    mouseY < 155 && 
-    mouseY > 135 )
-    {
-      fill(255,0,0)
-    }
-  else if (scene === 2 &&     
-    mouseX < 130 &&
-    mouseX > 110 &&
-    mouseY < 155 && 
-    mouseY > 135 )
-    {
-      fill(123, 0, 255)
-    }
-}
-
-function draw() {
-  //Call titlescreen
-  titlescreen();
-  background(0);
-  //draw food
-  foodcolor ();
-  rect(food.x, food.y, scl, scl);
-  }
-
-  if (s.eat(food)) {
-    pickLocation();
-  }
-
-  if (scene === 0) {
-    titlescreen();
-  } else if (scene === 1) {
-    s.death();
-    s.update();
-    s.show();
-  }
-  else if (scene === 2) {
-    MainMenu();
-  }
-  else if (scene === 3)
-    {
-      gameover();
-    }
 
 var mouseClicked = function () {
   if (
@@ -105,7 +62,96 @@ var mouseClicked = function () {
     {
       scene=0;
     }
+    if (scene === 2 &&     
+    mouseX < 95 &&
+    mouseX > 75 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+      r=255
+      g=0
+      b=0
+    }
+  else if (scene === 2 &&     
+    mouseX < 130 &&
+    mouseX > 110 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+     r=123
+     g=0
+     b=255
+    }
+    else if (scene === 2 &&     
+    mouseX < 165 &&
+    mouseX > 145 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+     r=0
+     g=174
+     b=255
+    }
+      else if (scene === 2 &&     
+    mouseX < 195 &&
+    mouseX > 175 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+     r=255
+     g=234
+     b=0
+    }
+        else if (scene === 2 &&     
+    mouseX < 230 &&
+    mouseX > 210 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+     r=255
+     g=51
+     b=0
+    }
+        else if (scene === 2 &&     
+    mouseX < 265 &&
+    mouseX > 245 &&
+    mouseY < 155 && 
+    mouseY > 135 )
+    {
+     r=43
+     g=255
+     b=0
+    }
 };
+function draw() {
+  //Call titlescreen
+  titlescreen();
+  background(0);
+  //draw food
+
+  fill (r,g,b);
+  rect(food.x, food.y, scl, scl);
+  
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+
+  if (scene === 0) {
+    titlescreen();
+  } else if (scene === 1) {
+    s.death();
+    s.update();
+    s.show();
+  }
+  else if (scene === 2) {
+    MainMenu();
+  }
+  else if (scene === 3)
+    {
+      gameover();
+    }
+}
 
 function keyPressed() {
   if (keyCode === UP_ARROW && s.yspeed != 1) {
